15e15debf90883cbffab3f8bf4d52ec79bc241e8
Harshit Kumar <kharshit020@gmail.com>
2024-08-28 17:03:05 +0530

    commit for 1.2

--- course-info/src/App.jsx
+++ course-info/src/App.jsx
@@ -6,21 +6,27 @@ const Header = (props)=>{
   )
 }
 
+const Part = (props) => {
+
+  return (
+
+    <>
+      <p>
+        {props.parts} {props.exercises}
+      </p>
+    </>
+
+  )
+
+}
 
 
 const Content = (props)=>{
   return (
     <>
-      <p>
-        {props.parts[0]} {props.exercises[0]}
-      </p>
-
-      <p>
-        {props.parts[1]} {props.exercises[1]}
-      </p>
-      <p>
-        {props.parts[2]} {props.exercises[2]}
-      </p>
+     <Part parts = {props.parts[0]} exercises = {props.exercises[0]} />
+     <Part parts = {props.parts[1]} exercises =  {props.exercises[1]} />
+     <Part parts = {props.parts[2]} exercises =  {props.exercises[2]} />
     </>
   )
 }
