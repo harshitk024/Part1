bab39ae63c7d938c97431dceef4da40f9282f1b1
Harshit Kumar <kharshit020@gmail.com>
2024-08-29 22:02:40 +0530

    commit for step 1

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -1,5 +1,13 @@
 import { useState } from 'react'
 
+const Button = ({onClickHandle,text}) => {
+     return (
+      <>
+       <button onClick = {onClickHandle}>{text}</button>
+      </>
+     )
+}
+
 
 
 const App = () => {
@@ -7,9 +15,29 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGood = () => {
+     setGood(good + 1)
+  }
+
+  const handleBad = () => {
+    setBad(bad + 1)
+  }
+
+  const handleNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+  
+
   return (
     <div>
-
+      <h1>Give Feedback</h1>
+      <Button onClickHandle = {handleGood} text = {"Good"} />
+      <Button onClickHandle = {handleNeutral} text = {"Neutral"} />
+      <Button onClickHandle = {handleBad} text = {"Bad"} />
+      <h1>statistics</h1>
+      <p>Good : {good}</p>
+      <p>Neutral : {neutral}</p>
+      <p>Bad : {bad}</p>
     </div>
   )
 }
